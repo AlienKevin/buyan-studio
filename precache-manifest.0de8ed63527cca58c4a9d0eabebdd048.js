9947bcc10d85b18e583ed140a247493ff312d15a
Kevin Li <kevinli020508@gmail.com>
2020-12-25 17:54:18 +0800

    Updates

---
+++ precache-manifest.0de8ed63527cca58c4a9d0eabebdd048.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/buyan-studio/favicon.ico"
+  },
+  {
+    "revision": "d72665935f7b40232e7e263333cc34d9",
+    "url": "/buyan-studio/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/buyan-studio/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/buyan-studio/manifest.json"
+  },
+  {
+    "revision": "35b998c7b4495d822b86",
+    "url": "/buyan-studio/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "83ce96882a354c1efdce",
+    "url": "/buyan-studio/static/js/10.83ce9688.chunk.js"
+  },
+  {
+    "revision": "23f9058fd6d62c2a7640",
+    "url": "/buyan-studio/static/js/11.23f9058f.chunk.js"
+  },
+  {
+    "revision": "c67e653d2521989f757d",
+    "url": "/buyan-studio/static/js/3.c67e653d.chunk.js"
+  },
+  {
+    "revision": "5293c0d7a00ded7ef24e",
+    "url": "/buyan-studio/static/js/4.5293c0d7.chunk.js"
+  },
+  {
+    "revision": "27990d51a0fd833f475b",
+    "url": "/buyan-studio/static/js/5.27990d51.chunk.js"
+  },
+  {
+    "revision": "aaf17e7aed55e431a8f7",
+    "url": "/buyan-studio/static/js/6.aaf17e7a.chunk.js"
+  },
+  {
+    "revision": "d226168d419ee00aaf21",
+    "url": "/buyan-studio/static/js/7.d226168d.chunk.js"
+  },
+  {
+    "revision": "a69c89721e4cdb99381a",
+    "url": "/buyan-studio/static/js/8.a69c8972.chunk.js"
+  },
+  {
+    "revision": "714c512807b4641acab0",
+    "url": "/buyan-studio/static/js/9.714c5128.chunk.js"
+  },
+  {
+    "revision": "35b998c7b4495d822b86",
+    "url": "/buyan-studio/static/js/main.35b998c7.chunk.js"
+  },
+  {
+    "revision": "0512f9e59f3dbc204535",
+    "url": "/buyan-studio/static/js/runtime~main.0512f9e5.js"
+  },
+  {
+    "revision": "1a0713cecf43bef4a46a",
+    "url": "/buyan-studio/static/js/vendors~main.1a0713ce.chunk.js"
+  }
+]);
